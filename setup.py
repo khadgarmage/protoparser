fbc2fc8d38c057c860dbe0cfe4cb5a3e13bda859
xiaochun.liu <xiaochun.liu@centurygame.com>
2020-06-14 07:49:33 +0800

    fix version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name="proto-parser",
-  version="1.0.0",
+  version="1.1.0",
   author="xiaochun.liu",
   author_email="liuxiaochun@apache.org",
   description="A package for parsing proto files",
