c6769fef747f95ba0665179fa660a9ad46960ea4
xiaochun.liu <xiaochun.liu@centurygame.com>
2020-06-16 00:18:49 +0800

    fix

--- protoparser/__init__.py
+++ protoparser/__init__.py
@@ -1,5 +1,5 @@
 from protoparser.parser import parse, parse_from_file, serialize2json_from_file
 from protoparser.parser import serialize2json
 
-__version__ = "1.2.1"
-__all__ = ['parse', 'parse_from_file', 'serialize2json', serialize2json_from_file]
+__version__ = "1.2.2"
+__all__ = [parse, parse_from_file, serialize2json, serialize2json_from_file]
