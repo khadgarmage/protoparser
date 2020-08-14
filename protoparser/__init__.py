d81a049d4ee9f3225b5950b89f979d7c9bafd521
xiaochun.liu <xiaochun.liu@centurygame.com>
2020-08-14 15:17:05 +0800

    fix protoparser comment

--- protoparser/__init__.py
+++ protoparser/__init__.py
@@ -1,5 +1,5 @@
 from protoparser.parser import parse, parse_from_file, serialize2json_from_file
 from protoparser.parser import serialize2json
 
-__version__ = "1.5.0"
+__version__ = "1.5.1"
 __all__ = [parse, parse_from_file, serialize2json, serialize2json_from_file]
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name="proto-parser",
-  version="1.5.0",
+  version="1.5.1",
   author="xiaochun.liu",
   author_email="liuxiaochun@apache.org",
   description="A package for parsing proto files",
